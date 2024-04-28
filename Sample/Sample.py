832fa2f89965fb45132678a297763f1544420f6f
bursoundie <bursoundie@gmail.com>
2024-04-28 00:43:01 -0400

    Cal-up-pipeline

--- Sample/Sample.py
+++ Sample/Sample.py
@@ -1,25 +1,25 @@
 # Calculator program in Python
 
 # Function to add two numbers
-def add(x, y):
-    return x + y
+def add(x, y, z):
+    return x + y + z
 
 
 # Function to subtract two numbers
-def subtract(x, y):
-    return x - y
+def subtract(x, y, z):
+    return x - y -z
 
 
 # Function to multiply two numbers
-def multiply(x, y):
-    return x * y
+def multiply(x, y, z):
+    return x * y * z
 
 
 # Function to divide two numbers
-def divide(x, y):
-    if y == 0:
+def divide(x, y, z):
+    if z == 0:
         return "Cannot divide by zero!"
-    return x / y
+    return (x / y) /z
 
 
 print("Select operation:")
@@ -34,14 +34,15 @@ choice = input("Enter choice (1/2/3/4): ")
 if choice in ('1', '2', '3', '4'):
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
+    num3 = float(input("Enter third number: "))
 
     if choice == '1':
-        print("Result:", add(num1, num2))
+        print("Result:", add(num1, num2, num3))
     elif choice == '2':
-        print("Result:", subtract(num1, num2))
+        print("Result:", subtract(num1, num2, num3))
     elif choice == '3':
-        print("Result:", multiply(num1, num2))
+        print("Result:", multiply(num1, num2, num3))
     elif choice == '4':
-        print("Result:", divide(num1, num2))
+        print("Result:", divide(num1, num2, num3))
 else:
     print("Invalid input")
